751ec7e3cf7c63344cbabaeeea9bfeb8f687332e
Prof-anabi <113741286+Prof-anabi@users.noreply.github.com>
2025-06-29 23:03:51 +0000

    Updated LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -227,5 +227,15 @@ export const LoginForm: React.FC = () => {
         </div>
       </div>
     </div>
+    <div className="mt-8 text-center">
+  <a href="https://bolt.new" target="_blank" rel="noopener noreferrer" className="inline-block">
+    <img
+      src="https://drive.google.com/file/d/1jhrXZgOTIVq9g1xNTibuQmJwe5QTqmqO/view?usp=drive_link" // Placeholder URL
+      alt="Powered by Bolt.new"
+      className="h-10"
+    />
+  </a>
+</div>
+
   );
 };
